1ee509b43abfd85f54d513f26496253b0f3ff782
vivek pradhan <vivekpradhan.91@gmail.com>
2023-07-20 09:37:39 +0530

    Issue 6: Map Keys to Nodes

---
+++ src/node.rs
@@ -0,0 +1,11 @@
+pub struct Node{
+    id: u64,
+}
+
+impl Node{
+    pub fn new(id: u64) -> Self{
+        Node{
+            id
+        }
+    }
+}
--- src/test.rs
+++ src/test.rs
@@ -1,17 +1,19 @@
 #[cfg(test)]
 mod tests {
     use crate::models::KVStore;
-
+    use crate::node::Node;
 
     #[test]
     fn test_create_kv_store(){
-        let kv_store = KVStore::new();
+        let nodes = vec![Node::new(1), Node::new(2), Node::new(3)];
+        let kv_store = KVStore::new(nodes);
         assert_eq!(kv_store.store.len(), 0);
     }
 
     #[test]
     fn test_put(){
-        let mut kvstore = KVStore::new();
+        let nodes = vec![Node::new(1), Node::new(2), Node::new(3)];
+        let mut kvstore = KVStore::new(nodes);
         kvstore.put("key".to_string(), "value".to_string());
         assert_eq!(kvstore.store.len(), 1);
         assert_eq!(kvstore.store.get("key"), Some(&"value".to_string()));
@@ -19,7 +21,8 @@ mod tests {
 
     #[test]
     fn test_get(){
-        let mut kv_store = KVStore::new();
+        let nodes = vec![Node::new(1), Node::new(2), Node::new(3)];
+        let mut kv_store = KVStore::new(nodes);
         kv_store.put("key".to_string(), "value".to_string());
         assert_eq!(kv_store.get("key"), Some(&"value".to_string()));
         assert_eq!(kv_store.get("key2"), None);
@@ -28,7 +31,8 @@ mod tests {
 
     #[test]
     fn test_delete(){
-        let mut kv_store = KVStore::new();
+        let nodes = vec![Node::new(1), Node::new(2), Node::new(3)];
+        let mut kv_store = KVStore::new(nodes);
         kv_store.put("key".to_string(), "value".to_string());
         assert_eq!(kv_store.delete("key"), Some("value".to_string()));
         assert_eq!(kv_store.delete("key"), None);
@@ -40,5 +44,13 @@ mod tests {
         let hashed_key = crate::hasher::hash_key(key);
         assert!(hashed_key > 0);
 
+    }
+
+    #[test]
+    fn test_get_node_for_key(){
+        let nodes = vec![Node::new(1), Node::new(2), Node::new(3)];
+        let kv_store = KVStore::new(nodes);
+        let node = kv_store.get_node_for_key("key");
+        assert!(node.is_some());
     }
 }
