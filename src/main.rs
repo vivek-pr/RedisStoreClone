bb2fa7271df457842793d9b1ae78e7f9b6062e89
vivek pradhan <vivekpradhan.91@gmail.com>
2023-07-24 19:19:26 +0530

    ssue #21: Implement the API Server #40

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,29 @@ mod test;
 mod hasher;
 mod node;
 mod expiration;
+mod test_main;
 
-fn main() {
-    println!("Hello, world!");
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
+    let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
+    let make_service = make_service_fn(|_conn| {
+        async {
+            Ok::<_, hyper::Error>(service_fn(handle_request))
+        }
+    });
+
+    let server = Server::bind(&addr).serve(make_service);
+    println!("Listening on http://{}", addr);
+    server.await?;
+    Ok(())
+
+}
+
+async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, Infallible>{
+    Ok(Response::new(Body::from("Hello World")))
 }
